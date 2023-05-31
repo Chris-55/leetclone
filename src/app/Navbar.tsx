453dc9289f3b88f419aa5338c0f20654d3603037
MariusDevelops <brazasmarius@gmail.com>
2023-05-31 14:29:57 +0300

    feat: add login and register links to navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <span className="text-white font-bold text-xl">Main</span>
+    <nav className='bg-gray-800'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-between h-16'>
+          <div className='flex items-center'>
+            <Link href='/'>
+              <span className='text-white font-bold text-xl'>Home</span>
+            </Link>
           </div>
-          <div className="flex">
-            <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-              Login
-            </span>
+          <div className='flex justify-right'>
+            <div className='flex'>
+              <Link href='/login'>
+                <span className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium mr-8'>
+                  Login
+                </span>
+              </Link>
+            </div>
+            <div className='flex'>
+              <Link href='/register'>
+                <span className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
+                  Register
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import Navbar from './Navbar'
+import Image from 'next/image';
+import Navbar from './Navbar';
 
 //Cia (speju) tas pats kas sjp reacte butu Index.js filas.
 
 export default function Home() {
   return (
-    <main className="">
+    <main className=''>
       <div>page</div>
     </main>
-  )
+  );
 }
